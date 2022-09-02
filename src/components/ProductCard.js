445cfda565d4864011c074da081cdc1d8ed5cba8
John Canero <johncanero.official@gmail.com>
2022-09-02 22:11:59 +0800

    Mobile Styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,14 +4,15 @@ import ImageMobile from '../images/image-product-mobile.jpg'
 const ProductCard = () => {
   return (
     <div>
-        <div className=' max-w-lg bg-white border-gray-200 shadow-md dark:border-gray-700 items-center'>
+        <div className=' max-w-lg bg-white dark:border-gray-700 items-center'>
 
-            {/* Anchor Tag */}
+            {/* Header Image */}
             <a href='/'>
                 <img className='' src={ImageMobile} alt=''></img>
             </a>
 
-            <div className='p-5'>
+            {/* Main Content */}
+            <div className='p-6'>
                 <h3 className='mt-4 font-Montserrat font-semibold text-darkGrayishBlue tracking-widest'>
                     PREVIEW
                 </h3>
@@ -20,7 +21,7 @@ const ProductCard = () => {
                     <h1 className='mt-8 mb-6 text-4xl tracking-tight text-darkBlue font-Fraunces '>Gabrielle Essence Eau De Parfum</h1>
                 </a>
 
-                <p className="mb-10 font-normal text-darkGrayishBlue ">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+                <p className="mb-10 font-normal  font-Montserrat text-darkGrayishBlue ">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
                 
                 <div className='flex space-x-6 items-center'>
                     <h1 className='text-4xl font-Fraunces text-greenJade'>$149.99</h1>
@@ -28,12 +29,20 @@ const ProductCard = () => {
                 </div>
 
                 <div className=' flex items-center justify-center'>
-                    <button className=" mt-6 w-full h-12 px-6 text-white transition-colors duration-150  bg-greenJade rounded-lg focus:shadow-outline hover:bg-green-800">
-                      Add to Cart
+                    <button className=" mt-6 w-full h-14 px-6 text-white transition-colors duration-150  bg-greenJade rounded-lg focus:shadow-outline hover:bg-green-800 shadow-md">
+                    <i class="fa-solid fa-cart-shopping"></i> Add to Cart
                     </button>
                 </div>
-               
             </div>
+
+            {/* Footer */}
+            <footer className="flex flex-col w-full h-16 bg-lightCream border-t-2 fixed border-white  left-0 bottom-0 justify-center items-center text-darkBlue">
+                    <p className=' font-Montserrat font-semibold'>Challenge by: <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>Frontend Mentor</a>.</p>
+
+                    <p className=' font-Montserrat'>Code by:
+                        <a href="/"> John Cañero</a>. 
+                    </p>
+            </footer>
         </div>
     </div>
   )
