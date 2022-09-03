59b8f28af869beab2661771065a33aeda7fcb433
John Canero <johncanero.official@gmail.com>
2022-09-03 12:23:34 +0800

    Footer Styling Colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <div> 
         {/* Footer */}
         <footer className="flex flex-col w-full h-16 bg-white border-t-2 fixed border-white  left-0 bottom-0 justify-center items-center text-black">
-                <p className=' font-Montserrat font-semibold'>Challenge by: <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>Frontend Mentor</a>.</p>
+                <p className=' font-Montserrat font-semibold text-darkBlue'>Challenge by: <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>Frontend Mentor</a>.</p>
 
-                <p className=' font-Montserrat'>Code by:
+                <p className=' font-Montserrat text-greenJade'>Code by:
                     <a href="/"> John Cañero</a>. 
                 </p>
         </footer>
